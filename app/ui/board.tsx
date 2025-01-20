6d36a3591b2e83746a671f7d07b3cd791801eed5
flamas472 <federicolamas95@gmail.com>
2025-01-20 10:55:03 -0300

    added imports and playGrid to manage interaction and behavior

--- app/ui/board.tsx
+++ app/ui/board.tsx
@@ -1,31 +1,31 @@
+"use client"
 import Tile from "@/app/ui/tiles";
 // import { tileValue } from "@/app/ui/tile";
-import {gameBoard} from "@/app/lib/gameboard";
-// import { useState } from "react";
+import { position, playerAction, gameState, gameBoard, playBoard, play} from "@/app/lib/gameboard";
+import { useState } from "react";
 
  
 
 export default function Board({columns, rows, mines}: {columns: number, rows: number, mines: number}) {
     
-    //const columns: number = 9;
-    //const rows: number = 9;
     const grid: number[][] = gameBoard(columns, rows, mines);
+    const [playGrid, setPlayGrid] = useState(playBoard(columns, rows));
 
 
     return(
         <div className="flex justify-center items-center flex-col gap-0">
             {
                 grid.map(
-                    (row, rIndex) => (
-                        <div key={rIndex} className="flex justify-center items-center flex-row gap-0 ">
+                    (row, y) => (
+                        <div key={y} className="flex justify-center items-center flex-row gap-0 ">
                             {
                                 row.map(
-                                    (tileValue, cIndex) => (
+                                    (tileValue, x) => (
                                         <Tile
                                             value={String(tileValue)}
                                             isHidden={false}
                                             isFlagged={false}
-                                            key={cIndex}
+                                            key={x}
                                         />
                                     )
                                 )
